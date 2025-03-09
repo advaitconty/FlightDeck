de0191cd3a5bf468840420074df405313dd48ea8
Advait Contractor <advaitthepro@outlook.com>
2025-03-09 21:22:19 +0800

    oops

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='flightdeck',
-    version='10.0.2',
+    version='10.0.3',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
